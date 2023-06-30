16a748eb22200a62b81dce36a5044502862bb49f
Emma Gerigscott <elgerig@gmail.com>
2023-06-30 10:35:31 -0700

    add conditional to show out of stock

--- src/components/Corn.js
+++ src/components/Corn.js
@@ -2,13 +2,22 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function Corn(props) {
+  let earInformation = null;
+  if (props.ears > 0) {
+    earInformation = 
+    <React.Fragment>
+      Ears available: {props.ears} 
+      </React.Fragment>} else {
+      earInformation =
+      <em>Out of stock</em>
+    }
   return (
     <div>
       <div onClick = {() => props.whenCornClicked(props.id)}>
         <h4>{props.name}</h4>
         <p>Origin: {props.origin}</p>
         <p>${props.price}</p>
-        <p>Ears in Stock: {props.ears}</p>
+        <p>{earInformation}</p>
       </div>
     </div>
   );
--- src/components/CornDetail.js
+++ src/components/CornDetail.js
@@ -2,13 +2,24 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function CornDetail(props){
+  let earInformation = null;
+  if (props.corn.ears > 0) {
+    earInformation = 
+    <React.Fragment>
+      Ears available: {props.corn.ears} 
+      <button onClick={() => props.sellCorn(props.corn.id)}>Sell Corn</button>
+      </React.Fragment>} else {
+      earInformation =
+      <em>Out of stock</em>
+    }
+  
   return (
     <div>
       <h1>{props.corn.name}</h1>
       <ul>
         <li>Origin: {props.corn.origin}</li>
         <li>Price: ${props.corn.price}</li>
-        <li>Ears available: {props.corn.ears} <button onClick={() => props.sellCorn(props.corn.id)}>Sell Corn</button></li>
+        <li>{earInformation}</li>
       </ul>
     </div>
   );

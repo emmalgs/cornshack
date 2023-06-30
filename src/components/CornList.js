e9ecbd11db5ea967e8c640ac3105a9cc3f240fd9
Emma Gerigscott <elgerig@gmail.com>
2023-06-30 09:18:02 -0700

    add cornlist outline

---
+++ src/components/CornList.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Corn from "./Corn";
+import PropTypes from "prop-types";
+
+function CornList(props){
+  return (
+    <div>
+      {props.cornList.map((corn) =>
+      <Corn
+        whenCornClicked = {props.onCornSelection}
+        name={corn.name}
+        origin={corn.origin}
+        price={corn.price}
+        bushels={corn.bushels}
+        id={corn.id}
+        key={corn.id} />
+        )}
+    </div>
+  );
+}
+
+CornList.propTypes = {
+  cornList: PropTypes.array,
+  onCornSelection: PropTypes.func
+}
